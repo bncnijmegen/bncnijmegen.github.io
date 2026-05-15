a5015c7214ebc5070ccf41379b9430046c149e6b
Florian Krause <siebenhundertzehn@gmail.com>
2026-05-15 22:41:03 +0200

    Update config.py

--- _stuffpages/config.py
+++ _stuffpages/config.py
@@ -38,8 +38,7 @@ html_head = [
     '<meta name="author" content="$author">',
     '<meta name="description" content="$description">',
     '<link href="$style" rel="stylesheet" media="screen" />',
-    '<link rel="icon" href="$favicon" type="image/x-icon" />',
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@7.2.0/css/fontawesome.min.css">'
+    '<link rel="icon" href="$favicon" type="image/x-icon" />'
 ]
 
 # HTML navigation
